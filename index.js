44db2ccb7efba145bcf08ab63b85f9b779fe1054
Elizabeth Witengier <ewitengier@ufl.edu>
2022-05-22 17:51:09 -0400

    completed 2

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+function greet() {
+  var userName = prompt("What is your name?")
+  if (userName && userName.trim()) {
+    alert('Hello, ' + userName.trim())
+  } else {
+    alert('Hello')
+  }
+  var userAge = prompt("How old are you?")
+  console.log(parseInt(userAge))
+  var passed = confirm("Has your birthday already passed this year?")
+  var today = new Date()
+  var year = today.getFullYear()
+   if (passed){
+    alert("You were born in " + (year - userAge))
+   } else {
+     alert("You were born in " + ((year - 1) - userAge))
+   }
+  }
+  
+
+
+
+
+
+
+
+
+
